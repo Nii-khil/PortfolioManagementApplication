a839658fc57b22d7cd3d894a006f2f66e3ba893f
Nikhil Upadhyay <nikhilupadhyayblr@gmail.com>
2026-02-04 06:25:26 +0000

    Migrate frontend code outside static folder

--- src/main/java/org/jdbc/portfoliomanagement/controller/HoldingController.java
+++ src/main/java/org/jdbc/portfoliomanagement/controller/HoldingController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5500")
+@CrossOrigin(origins = "http://127.0.0.1:5500/")
 @RestController
 @RequestMapping("/api")
 public class HoldingController {
